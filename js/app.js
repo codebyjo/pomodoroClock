dc2f484eeb61afd954a8236bc5ac5787a86b2b86
Joanne D <codebyjo@gmail.com>
2017-06-15 20:57:24 -0400

    attemped adding time buttons

--- js/app.js
+++ js/app.js
@@ -65,9 +65,78 @@ $(document).ready(function () {
     sessionActive = false;
   }
 
+
   //switch between work and break times
   // when work finished, switch to break, vice versa
+  //play sound
+
+  function switchSession(playsound) {
+    if (playsound) {
+      var buzzer = new Audio('../buzzer.mp3');
+      buzzer.play();
+    }
+
+    if (sessionActive) {
+      timeLeft = breakLength;
+      leftoverSeconds = false;
+    } else {
+      leftoverSeconds = sessionLength;
+      sessionActive = true;
+    }
+  }
+
+
   //reset button
   //buzzer when timer finishes
 
+  // Displaying session length and break length.
+ 
+  function updateTimerLengths() {
+    if (sessionLength < 0) {
+      sessionLength = 0;
+    }
+
+    if (breakLength < 0) {
+      breakLength = 0;
+    }
+    $('#sessionNumber').html(sessionLength);
+    $('#breakNumber').html(breakLength);
+  }
+
+    //  Adjusting time on plus minus buttons
+
+  function iClickHandler(id) {
+    switch (id) {
+      case 'sessionPlus':
+        sessionlength + 1;
+        if (sessionActive) {
+          timeCopy + 1;
+        }
+        break;
+      case 'sessionMinus':
+        sessionlength - 1
+        if (sessionActive) {
+          timeCopy - 1;
+        }
+        break;
+      case 'breakPlus':
+        breakLength + 1;
+        if (!sessionActive) {
+          breakCopy + 1;
+        }
+        break;
+      case 'breakMinus':
+        breakLength - 1;
+        if (!sessionActive) {
+          breakCopy - 1;
+        }
+        break;
+    }
+
+    $('#sessionNumber').html(sessionLength);
+    $('#breakNumber').html(breakLength);
+    // Adjust timer
+  }
+
 });
+
